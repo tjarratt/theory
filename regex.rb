9d215d357b90e0d517ea22d9853784812462f652
Tim Jarratt <tjarratt@gmail.com>
2013-06-11 13:55:26 -0700

    First commit of regex parser
    Yeah, this is a huge mess and should be cleaned up. This initial stab worked
    for about 90% of the testcases but some of the more important ones would fail
    because we weren't able to handle multiple modifiers in a row.
    
    What this code should do is make a pass through the regex, look for interesting
    operators (replacing them with symbols or a proc returning true : false),
    and then we read through the string going through the operators, short
    circuiting if any of them fails

--- regex.rb
+++ regex.rb
@@ -10,37 +10,55 @@ def match?(expr, string)
   return false if escape_char == '+' #invalid regexp
 
   match_count = 0
+  # puts "matching #{string} against #{expr}"
 
-  string.chars.each do |c|
+  index = 0
+  while index < string.size
+    c = string[index]
+    # puts "at char #{c} with match count #{match_count}"
+    # puts "comparing #{c} to #{escape_char}"
+
     if c == escape_char
+      index += 1
+      match_count += 1
       previous = escape_char
       escape_char = expression.shift
     elsif escape_char == '.'
+      index += 1
+      match_count += 1
       previous = escape_char
       escape_char = expression.shift
     elsif escape_char == '*' # check for zero or more
       if previous == c || previous == '.'
+        index += 1
         match_count += 1
-        next
       else
+        index += 1
         match_count = 0
         previous = nil
         escape_char = expression.shift
       end
     elsif escape_char == '+' # check for one or more
       if previous == c || previous == '.'
+        index += 1
         match_count += 1
-        next
+        # puts "in +, incremented  match count to #{match_count}"
       else
         if match_count > 0
+          # puts "end of match with prev #{previous} and escape_char #{escape_char}"
           match_count = 0
           previous = nil
           escape_char = expression.shift
+          # puts "now previous is nil and escape_char is #{escape_char}"
         else
+          # puts "returning false with escape char +"
+          # puts "char: #{c}"
+          # puts "previous: #{previous}"
           return false
         end
       end
     else
+      # puts "returning false because #{c} != #{escape_char}"
       return false
     end
   end
@@ -55,22 +73,21 @@ def match?(expr, string)
 end
 
 puts "should pass:"
-puts match?('abc', 'abc')
-puts match?('a..', 'abc')
-puts match?('a*', 'aaa')
-puts match?('aa+', 'aaa')
-puts match?('a.*', 'abc')
-puts match?('.*g', 'abcdefg')
-puts match?('a.*', 'abcdefg')
-puts match?(".*", "")
+# puts match?('abc', 'abc')
+# puts match?('a..', 'abc')
+# puts match?('a*', 'aaa')
+# puts match?('aa+', 'aaa')
+# puts match?('a.*', 'abc')
+# puts match?('.*g', 'abcdefg')
+# puts match?('a.*', 'abcdefg')
+# puts match?(".*", "")
 
 # from glass door
-puts match?("abc", "abc")
 puts match?("a+b+c+", "abc")
 puts match?("a*b*c*", "abc")
-puts match?("abc*", "abc")
-puts match?("a..", "abc")
-puts match?("...", "abc")
+# puts match?("abc*", "abc")
+# puts match?("a..", "abc")
+# puts match?("...", "abc")
 puts match?("abcf*h", "abcffffffffffh")
 puts match?("abcf?e", "abce")
 puts match?("abcf*", "abc")
@@ -82,12 +99,12 @@ puts match?('abc', 'aaa')
 puts match?('+jkdljkd', 'aaa')
 puts match?(".+", "")
 
-# from glass door
+# # from glass door
 puts match?("def", "abc")
 puts match?("d+ef", "abc")
-puts match?("d*ef", "abc")
-puts match?("acdf+", "abc")
-puts match?("abcf*", "abcffffffffffh")
-puts match?("a*b*c*e+f+g+", "ef")
+# puts match?("d*ef", "abc")
+# puts match?("acdf+", "abc")
+# puts match?("abcf*", "abcffffffffffh")
+# puts match?("a*b*c*e+f+g+", "ef")
 
 puts "\n\nend\n\n\n"
