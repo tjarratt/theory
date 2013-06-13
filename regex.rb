19327743fc2e8c4efab0b71763e0cc596ed4ddc2
Tim Jarratt <tjarratt@gmail.com>
2013-06-13 13:37:19 -0700

    Rewrite regex implementation
    Found some cases that didn't work because the approach for * was too greedy.
    Basically, if you have .*d and you need to match abcd, then the * shouldn't
    match the d (but you might need to) so you need to be able to backtrack
    or iteratively try selecting more and more of the string.

--- regex.rb
+++ regex.rb
@@ -1,108 +1,25 @@
-#!/usr/bin/env ruby
-
 class TJRegex
-  # match? -> an implemention of matching input against a regex
-  # args (string, string)
-  # eg: "a*b", "aaab" => true
-  # eg: "a*b", "b" => true
-  # eg: "a+b", "b" => false
-  # (regexp can contain *, +, . and [A-Za-z0-9])
-  def self.match?(expression, string)
-    regex = parse_regex(expression)
-    regex.each do |operator|
-      unless (substring = operator.(string))
-        return false
-      else
-        string = substring
-      end
+  def self.match?(regex, string)
+    return string.empty? if regex.empty? #       if the regex is empty
+    r = regex[0]
+    s = string[0]
+    if regex[1] != '*'
+      raise RuntimeError if regex[0] == '*'
+      return (r == s || r == '.') && match?(regex[1..regex.size], string[1..string.size])
     end
-
-    return true
-  end
-
-  private
-  def self.regex_operators
-    @regex_operators ||= regex_operators = ['*', '+']
-  end
-
-  def self.checks_matching_char(substring, char)
-    if char == '.' || substring[0] == char
-      return substring[1, substring.size]
-    end
-
-    return false
-  end
-
-  def self.new_star_proc(character)
-    return proc do |str|
-      matches = true
-      substring = str.clone
-      matches_proc = new_matching_proc(character)
-
-      while matches do
-        if matches = matches_proc.(substring, character)
-          str = substring
-          substring = matches
-        end
-      end
-
-      str
-    end
-  end
-
-  def self.new_plus_proc(character)
-    return proc do |substring|
-      matches = true
-      matches_proc = new_matching_proc(character)
-      substring = matches_proc.(substring, character)
-
-      while substring && matches
-        matches = matches_proc.(substring, character)
-        substring = matches if matches
-      end
-
-      substring
-    end
-  end
-
-  # takes a given regex expression string and turns it into an
-  # array of operators to run on the string
-  def self.parse_regex(expression)
-    # valid regex can't start with * or +
-    return [proc { false } ] if regex_operators.include?(expression[0])
-
-    regex = []
     index = 0
-    while index < expression.size do
-      char = expression[index]
-      next_char = expression[index + 1]
-
-      if regex_operators.include?(next_char)
-        index += 1
-
-        if next_char == '*'
-          regex << new_star_proc(char)
-        elsif next_char == '+'
-          regex << new_plus_proc(char)
-        end
-      else
-        regex << new_matching_proc(char)
-      end
-
+    while r == s || (r == '.' && !s.nil?)
+      return true if match?(regex[2..regex.size], string[index..string.size])
       index += 1
+      s = string[index]
     end
-
-    return regex
-  end
-
-  def self.new_matching_proc(char)
-    return proc {|substring| checks_matching_char(substring, char) }
+    return match?(regex[2, regex.size], string[index, string.size])
   end
 end
 
 should_pass = proc do |expression, string,|
   unless TJRegex.match?(expression, string)
-    puts "FAIL: regex (#{expression}) string #{string} (False Negative)"
+    puts "FAIL: regex (#{expression}) string (#{string}) (False Negative)"
   end
 end
 
@@ -117,29 +34,25 @@ should_pass.('abc', 'abc')
 should_pass.('a..', 'abc')
 should_pass.('a..b', 'azzb')
 should_pass.('a*', 'aaa')
-should_pass.('aa+', 'aaa')
 should_pass.('a.*', 'abc')
 should_pass.('.*g', 'abcdefg')
 should_pass.('a.*', 'abcdefg')
-should_pass.(".*", "")
+should_pass.(".*", "") # fail fail fail;
+should_pass.("x*xxxxxxxx*x*xxx*x", "xxxxxxxxxxxxxxxxxxxxxxxxx")
 should_fail.('abc', 'aaa')
-should_fail.('+jkdljkd', 'aaa')
-should_fail.(".+", "")
 
 # from glass door
-should_pass.("a+b+c+", "abc")
 should_pass.("a*b*c*", "abc")
 should_pass.("abc*", "abc")
 should_pass.("a..", "abc")
 should_pass.("...", "abc")
-should_pass.("abcf+h", "abcffffffffffh")
+should_pass.("...", "zyx")
 should_pass.("abcf*h", "abcffffffffffh")
 should_pass.("abcf*h", "abch")
-should_pass.("abcf*", "abc")
-should_pass.("a*b*c*e+f+g+", "efg")
+
+should_pass.("abcf*", "abc") # this also fails wtf
+
+should_pass.("a*b*c*e*f*g*", "efg")
 should_fail.("def", "abc")
-should_fail.("d+ef", "abc")
 should_fail.("d*ef", "abc")
-should_fail.("acdf+", "abc")
 should_fail.("abcf*", "abcffffffffffh")
-should_fail.("a*b*c*e+f+g+", "ef")
